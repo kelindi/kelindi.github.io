b20abd4c8ddb765cf23dc8248605648691733de5
kelindi <ayushk2020@gmail.com>
2021-01-02 18:29:13 -0400

    form test

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
 	$to = 'ayushk2020@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$from = 'ayushk2020@gmail.com'; // Admin e-mail
+	$fromName = 'Kelindi'; // Your company name
 	$charset = 'UTF-8';
 ?>
